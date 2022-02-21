02958a82b241bfe7072531ef7427e099f3432748
Tony Bowden <tony@tmtm.com>
2022-02-21 14:45:50 +0200

    compare enwiki Senate Presidents

---
+++ sources/senate-president/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Party'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[col no name party start end].freeze
+    end
+
+    def empty?
+      tds[0].text == tds[1].text
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
