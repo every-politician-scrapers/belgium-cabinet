203b271f4e3da346449fcecb0b81d7fcd87ab67d
Tony Bowden <tony@tmtm.com>
2022-04-27 08:03:07 +0300

    adjust scraper for layout changes

--- sources/bank-governor/scraper.rb
+++ sources/bank-governor/scraper.rb
@@ -4,31 +4,46 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
+class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
+  def empty?
+    false
+  end
+
+  def combo_date?
+    true
+  end
+
+  def raw_combo_date
+    raise 'need to define a raw_combo_date'
+  end
+
+  def name_node
+    raise 'need to define a name_node'
+  end
+end
+
+
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
   decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  def header_column
-    'Governors'
-  end
-
   # TODO: make this easier to override
   def holder_entries
-    noko.xpath("//h2[.//span[contains(.,'#{header_column}')]][1]//following-sibling::ul[1]//li[a]")
+    noko.xpath("//h2[.//span[contains(.,'Governors')]][1]//following-sibling::ul[1]//li[a]")
   end
 
-  class Officeholder < OfficeholderBase
-    def combo_date?
-      true
-    end
-
+  class Officeholder < OfficeholderNonTable
     def raw_combo_dates
       noko.text[/\((.*?)\)/, 1].split(/[-–]/).map(&:tidy)
     end
 
-    def name_cell
-      noko.xpath('a')
+    def name_node
+      noko.css('a').first
+    end
+
+    def item
+      noko.css('a/@wikidata').map(&:text).last
     end
   end
 end
